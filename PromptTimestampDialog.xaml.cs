a71a53620d4c68a7406c71a2cc42d92e8168dfe7
ViviMayoi <31770157+ViviMayoi@users.noreply.github.com>
2023-05-30 14:05:58 -0400

    Improved user input for timestamps by separating hours, minutes and seconds

--- PromptTimestampDialog.xaml.cs
+++ PromptTimestampDialog.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace OverlayControl
@@ -8,7 +9,7 @@ namespace OverlayControl
     /// </summary>
     public partial class PromptTimestampDialog : Window
     {
-        public string GivenTime { get => txtVodTime.Text; }
+        public string GivenTime { get => txtHours.Text + ":" + txtMinutes.Text + ":" + txtSeconds.Text; }
 
         /// <summary>
         /// Constructor for the PromptTimestampDialog window, acting as a prompt for the user to enter a timestamp.
@@ -17,6 +18,53 @@ namespace OverlayControl
         {
             InitializeComponent();
         }
+
+        #region Text Boxes
+        /// <summary>
+        /// Event fired when a text box that should only contain numbers is typed into.
+        /// </summary>
+        /// <param name="sender">The text box that fired this event.</param>
+        /// <param name="e">Contains state information and event data.</param>
+        private void txtNumber_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e) =>
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+
+        /// <summary>
+        /// Event fired when a text box that should only contain numbers is pasted into.
+        /// </summary>
+        /// <param name="sender">The text box that fired this event.</param>
+        /// <param name="e">Contains state information and event data.</param>
+        private void txtNumber_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            // Check if data is a string
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                // Check if the data string only contains numbers; if not, cancel
+                if (new Regex("[^0-9]+").IsMatch((string)e.DataObject.GetData(typeof(string))))
+                    e.CancelCommand();
+            }
+            // If data is not a string, cancel paste
+            else
+                e.CancelCommand();
+        }
+
+        /// <summary>
+        /// Event fired when a text box that should only contain numbers has data dragged down into it via mouse.
+        /// </summary>
+        /// <param name="sender">The text box that fired this event.</param>
+        /// <param name="e">Contains state information and event data.</param>
+        private void txtNumber_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+            // Check if data is a string
+            if (e.Data.GetDataPresent(typeof(string)))
+            {
+                // Check if the data string only contains numbers; if not, cancel
+                e.Handled = new Regex("[^0-9]+").IsMatch((string)e.Data.GetData(typeof(string)));
+            }
+            // If data is not a string, cancel drag-down
+            else
+                e.Handled = true;
+        }
+        #endregion
 
         /// <summary>
         /// Event fired when button btnFinalize is clicked.
@@ -36,7 +84,7 @@ namespace OverlayControl
         private void wndTimestampPrompt_ContentRendered(object sender, EventArgs e)
         {
             // Allow the user to type into the textbox immediately by putting it in focus when the window is rendered
-            txtVodTime.Focus();
+            txtHours.Focus();
         }
     }
 }
